1a7a310ca59602dfa45316cf1b0569fffb971af0
Ramin <RaminShamsi@yahoo.com>
2017-04-18 12:10:23 +0200

    add Copy.java file and the test file source.txt to check if the code works well

--- src/Copy.java
+++ src/Copy.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Copy {
   public static void main(String[] args) {
     // This should be the basic replica of the 'cp' command
@@ -9,9 +16,22 @@ public class Copy {
     // When both arguments provided and the source is a file
     // Read all contents from it and write it to the destination
 
-    if (args[0].equals("")) {
+    if (args.length == 0) {
       System.out.println("copy [source] [destination]");
+    } else if (args[0].equals("copy") && args.length == 1) {
+      System.out.println("No destination provided");
+    } else if (args[0].equals("copy") && args[1].equals("source.txt") && args[2].equals("destination.txt")) {
+      Path source = Paths.get(args[1]);
+      Path destination = Paths.get(args[2]);
+      try {
+        List<String> lines = new ArrayList<>(Files.readAllLines(source));
+        for (String line : lines) {
+         byte[] buf = line.getBytes();
+         Files.write(destination, buf);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-    
   }
 }
